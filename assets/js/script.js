de143ea64092d3b755b4612851005db3cade3810
ehmedosmanov <ehmedosmanov@gmail.com>
2023-10-05 17:40:46 +0400

    Added project files

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// function fibonacci(n) {
+//   if (n <= 0) {
+//     return 0
+//   }
+//   if (n === 1) {
+//     return 1
+//   }
+
+//   return fibonacci(n - 1) + fibonacci(n - 2)
+// }
+
+// const result = fibonacci(2)
+
+// console.log(result)
+
+// number = 121 = true
+// number = 122 = false
+
+// function isPalindrome(number) {
+//   const str = number.toString();
+//   const reversedStr = str.split("").reverse().join("");
+
+//   return str === reversedStr;
+// }
+
+// console.log(1221);
